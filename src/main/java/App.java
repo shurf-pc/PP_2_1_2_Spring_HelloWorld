eec13a59578800a2ed0f9933a021fa064fcfc3a2
Pavel Ripanov <shurf.pc@gmail.com>
2024-11-24 12:41:22 +0300

    Add class Cat and prototype beans "cat"

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -7,6 +7,11 @@ public class App {
                 new AnnotationConfigApplicationContext(AppConfig.class);
         HelloWorld bean =
                 (HelloWorld) applicationContext.getBean("helloworld");
-        System.out.println(bean.getMessage());
+        HelloWorld bean2 = (HelloWorld) applicationContext.getBean("helloworld");
+        System.out.println("Is bean==bean2? : " + (bean == bean2));
+        Cat cat = (Cat) applicationContext.getBean("cat");
+        Cat cat2 = (Cat) applicationContext.getBean("cat");
+        System.out.println("Is cat==cat2? : " + (cat == cat2));
+
     }
 }
